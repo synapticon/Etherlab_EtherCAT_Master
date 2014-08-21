22919a7058468458b434bbe5b2217f2646316b20
Knud Baastrup <kba@deif.com>
2014-08-21 15:11:28 +0200

    Sdo directory now only fetched on request.
    The time-consuming SDO directory fetch during slave scan can now be
    skipped by setting the EC_SKIP_SDO_DICT in globals.h. The directory
    will now instead be fethed the first time an ethercat sdos command
    is executed with the ethercat tool.

--- master/ioctl.c
+++ master/ioctl.c
@@ -4421,6 +4421,28 @@ static ATTRIBUTES int ec_ioctl_slave_soe_write(
 }
 
 /*****************************************************************************/
+/** Upload Dictionary.
+ *
+ * \return Zero on success, otherwise a negative error code.
+ */
+static ATTRIBUTES int ec_ioctl_slave_dict_upload(
+        ec_master_t *master, /**< EtherCAT master. */
+        void *arg /**< ioctl() argument. */
+        )
+{
+    ec_ioctl_slave_dict_upload_t data;
+    int ret;
+
+    if (copy_from_user(&data, (void __user *) arg, sizeof(data))) {
+        return -EFAULT;
+    }
+
+    ret = ec_master_dict_upload(master, data.slave_position);
+
+    return ret;
+}
+
+/*****************************************************************************/
 
 /** ioctl() function to use.
  */
@@ -4974,6 +4996,9 @@ long EC_IOCTL(
                 break;
             }
             ret = ec_ioctl_set_send_interval(master, arg, ctx);
+            break;
+        case EC_IOCTL_SLAVE_DICT_UPLOAD:
+            ret = ec_ioctl_slave_dict_upload(master, arg);
             break;
         default:
             ret = -ENOTTY;
