59ca743e4770deef3099b541b71238d63781d9ca
Gavin Lambert <gavinl@compacsort.com>
2017-11-02 17:14:45 +1300

    Make rejects from update scripts easier to interpret.

--- devices/e1000e/update.sh
+++ devices/e1000e/update.sh
@@ -6,7 +6,7 @@ KERNELDIR=/data/kernel/linux-3.16.6
 PREVER=3.14
 KERNELVER=3.16
 
-for f in $KERNELDIR/drivers/net/ethernet/{realtek/8139too,realtek/r8169,intel/e100}.c; do
+for f in $KERNELDIR/drivers/net/ethernet/intel/e1000e/*.[ch]; do
     echo $f
     b=$(basename $f)
     o=${b/\./-$KERNELVER-orig.}
@@ -16,6 +16,7 @@ for f in $KERNELDIR/drivers/net/ethernet/{realtek/8139too,realtek/r8169,intel/e1
     cp -v $o $e
     op=${b/\./-$PREVER-orig.}
     ep=${b/\./-$PREVER-ethercat.}
-    diff -u $op $ep | patch -p1 $e
+    diff -up $op $ep | patch -p1 $e
+    sed -i s/$PREVER-ethercat.h/$KERNELVER-ethercat.h/ $e
     hg add $o $e
 done
