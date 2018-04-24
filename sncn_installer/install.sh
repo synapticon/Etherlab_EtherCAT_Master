79de4735302fd3c1d0109067f1bc9f1ae88e60c2
David Bensoussan <minipada@gmail.com>
2018-04-24 18:36:04 +0200

    feat: add custom install script

---
+++ sncn_installer/install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -o errexit
+set -o verbose
+
+# Setup parameters
+ETHERCAT_USER_GROUP="$(whoami)"
+UDEV_RULES_FILE="/etc/udev/rules.d/99-EtherCAT.rules"
+SCRIPT_DIR="$(cd "$( dirname "$0" )" && pwd)"
+ETHERCAT_SYSCONFIG="/etc/sysconfig/ethercat"
+ETHERCAT_INSTALL_PREFIX=""
+ETHERCAT_START_PREFIX="/opt/etherlab"
+
+do_configure() {
+  cd $SCRIPT_DIR/..
+  ./bootstrap
+  ./configure --enable-sii-assign --disable-8139too --enable-hrtimer --enable-cycles --prefix=$ETHERCAT_INSTALL_PREFIX
+
+  if [[ -f "$ETHERCAT_INSTALL_PREFIX/etc/init.d/ethercat" ]]; then
+    sudo $ETHERCAT_INSTALL_PREFIX/etc/init.d/ethercat stop
+  elif [[ -f "$ETHERCAT_START_PREFIX/etc/init.d/ethercat" ]]; then
+    sudo $ETHERCAT_START_PREFIX/etc/init.d/ethercat stop
+  fi
+}
+
+do_compile () {
+  make clean
+  make all modules
+}
+
+do_install () {
+  sudo make modules_install install
+  sudo ldconfig
+  sudo depmod
+}
+
+do_setup_interfaces () {
+  # Add udev rule
+  sudo rm -rf $UDEV_RULES_FILE
+  echo "KERNEL==\"EtherCAT[0-9]*\", MODE=\"0664\", GROUP=\"$ETHERCAT_USER_GROUP\"" | sudo tee --append $UDEV_RULES_FILE
+
+  # Add detected interfaces and delete old ones
+  sudo sed -i '/MASTER.*DEVICE/d' /etc/sysconfig/ethercat
+  iface_index="0"
+  interfaces=$(ifconfig | grep -e "^e[tn][a-z0-9]*" -o)
+
+  # Intel Up square 2
+  if [[ -n $(lscpu | sed '13q;d' | grep "Intel(R) Pentium(R) CPU N4200 @ 1.10GHz") ]];then
+    MAC=$(cat /sys/class/net/enp3s0/address)
+    echo "MASTER${iface_index}_DEVICE=\"$MAC\"" | sudo tee --append $ETHERCAT_SYSCONFIG
+  else
+  # Standard laptop
+    for iface in $interfaces ; do
+      MAC=$(cat /sys/class/net/$iface/address)
+      echo "MASTER${iface_index}_DEVICE=\"$MAC\"" | sudo tee --append $ETHERCAT_SYSCONFIG
+      iface_index=$((iface_index+1))
+    done
+  fi
+  sudo sed -i 's/DEVICE_MODULES=\"\"/DEVICE_MODULES=\"generic\"/g' $ETHERCAT_SYSCONFIG
+}
+
+do_start() {
+  if [[ -f "$ETHERCAT_INSTALL_PREFIX/etc/init.d/ethercat" ]]; then
+    sudo $ETHERCAT_INSTALL_PREFIX/etc/init.d/ethercat start
+  elif [[ -f "$ETHERCAT_START_PREFIX/etc/init.d/ethercat" ]]; then
+    sudo $ETHERCAT_START_PREFIX/etc/init.d/ethercat start
+  fi
+}
+
+do_configure
+do_compile
+do_install
+do_setup_interfaces
+do_start
+
+echo "Done"
